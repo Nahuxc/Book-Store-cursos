f4ae1313c8ce4ebce96efd38316f754fe1944723
Nahuel <coronel98nahuel@gmail.com>
2022-07-16 01:27:22 -0300

    version1.5.3

--- js/main.js
+++ js/main.js
@@ -4,19 +4,16 @@ const sidebar = document.getElementById("sidebar")
 const menu = document.getElementById("menu")
 const btndark = document.getElementById("btndark")
 
-
 /* function of the darkMode */
 btndark.addEventListener("click",()=>{
     document.body.classList.toggle("dark-mode")
 })
 
 /* function of the submenu */
-
 btnmenu.addEventListener("click", ()=>{
     btnmenu.classList.toggle("deploy")
     sidebar.classList.toggle("active")
 })
-
 
 /* scroll NavBar Effect */
 window.addEventListener("scroll", ()=>{
