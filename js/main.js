0c63b249fd980e7b34b978ef9a51d0f10d042524
Nahuel <coronel98nahuel@gmail.com>
2022-07-27 00:52:14 -0300

    version1.5.7

--- js/main.js
+++ js/main.js
@@ -5,6 +5,42 @@ const menu = document.getElementById("menu");
 const btndark = document.getElementById("btndark");
 const logo = document.getElementById("logo");
 const logo2 = document.getElementById("logo2");
+const cardTemplate = document.getElementById("templateCard").content
+const fragment = document.createDocumentFragment()
+
+/* DOM objects loaded */
+document.addEventListener("DOMContentLoaded", ()=>{
+    dataFetch()
+})
+
+/* fetch */
+const dataFetch = async()=>{
+    try {
+        /* API */
+        const res = await fetch('api.json');
+        const data = await res.json();
+        createCard(data);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+/* create card product */
+const createCard = data =>{
+    data.forEach(prod => {
+        cardTemplate.querySelector('.coursesImg').setAttribute("src", prod.img);
+        cardTemplate.querySelector('.coursesTextCategory').textContent = prod.category;
+        cardTemplate.querySelector('.coursesTituleH2').textContent = prod.name;
+        cardTemplate.querySelector('.coursesText').textContent = `$ `+ prod.price;
+        cardTemplate.querySelector('.coursesTextClock').innerHTML = `<i class="fa-solid fa-clock"></i> ` + prod.hours;
+        cardTemplate.querySelector('.coursesTextVideo').innerHTML = `<i class="fa-solid fa-video"></i> ` + prod.video;
+        cardTemplate.querySelector('.btnbuy').dataset.id = prod.id;
+        const clone = cardTemplate.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+    card.appendChild(fragment)
+
+}
 
 /* function of the darkMode */
 btndark.addEventListener("click",()=>{
